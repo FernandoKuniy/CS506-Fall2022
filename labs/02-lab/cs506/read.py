53ddf0a7aca8dffd670c6987115b0d62fa232851
Fernando Kuniy <kuniy@bu.edu>
2022-09-29 15:52:42 -0400

    test

--- labs/02-lab/cs506/read.py
+++ labs/02-lab/cs506/read.py
@@ -1,6 +1,15 @@
+from csv import reader
+
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+
+    # read csv file as a list of lists
+    with open(csv_file_path, 'r') as read_obj:
+        # pass the file object to reader() to get the reader object
+        csv_reader = reader(read_obj)
+        # Pass reader object to list() to get a list of lists
+        list_of_rows = list(csv_reader)
+        return list_of_rows
